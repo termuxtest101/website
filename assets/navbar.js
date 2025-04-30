4f8167472c0139826825cfeac2a4c17c6332a6a0
Termux Test <99701913+termuxtest101@users.noreply.github.com>
2025-04-30 08:24:21 +0000

    modify some pages

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,18 +1,5 @@
 const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-const osMenu = document.getElementById('osMenu');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('show');
-});
-
-osMenu.addEventListener('click', (e) => {
-  e.stopPropagation();
-  osMenu.classList.toggle('show');
-});
-
-document.addEventListener('click', (e) => {
-  if (!osMenu.contains(e.target)) {
-    osMenu.classList.remove('show');
-  }
 });
